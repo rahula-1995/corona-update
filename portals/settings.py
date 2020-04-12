727b013095481f2a3a97510133cc19f141a71dc5
rahula-1995 <49114833+rahula-1995@users.noreply.github.com>
2020-04-13 00:48:05 +0530

    Update settings.py

--- portals/settings.py
+++ portals/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = '510knwtqc%qs%nvo#0%tc^yj0&w7q8w-6c$u&br^cm3ck=c%i8'
+
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
